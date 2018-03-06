7aaf05fa1d5367db19f110a9ddbb188999d0efdc
mayank1495 <mayank007.win@gmail.com>
2018-03-06 23:58:35 +0530

    Added geo and weather url

--- app-promise.js
+++ app-promise.js
@@ -1,3 +1,22 @@
 const yargs = require('yargs');
 const axios = require('axios');
 
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather.',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+var geoAPIurl = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
+var passedAdd = encodeURIComponent(argv.address);
+var geoURL = geoAPIurl + passedAdd;
+var weatherAPIurl = 'https://api.darksky.net/forecast';
+var APIkey = 'd087458df0d4a8c2c58b7c62f8708782';
+var weatherURL = `${weatherURL}/${APIkey}/`;
