0e6ab7d6bd01057462eb74490732cbd2e98b53e6
mayank1495 <mayank007.win@gmail.com>
2018-03-07 00:14:59 +0530

    Use promises for fetching weather data

--- app-promise.js
+++ app-promise.js
@@ -20,3 +20,19 @@ var geoURL = geoAPIurl + passedAdd;
 var weatherAPIurl = 'https://api.darksky.net/forecast';
 var APIkey = 'd087458df0d4a8c2c58b7c62f8708782';
 var weatherURL = `${weatherAPIurl}/${APIkey}/`;
+
+axios.get(geoURL).then((response) => {
+	if (response.data.status != 'OK') {
+		throw new Error('Some Error..');
+	}
+	var lat = response.data.results[0].geometry.location.lat;
+	var lng = response.data.results[0].geometry.location.lng;
+	weatherURL = `${weatherURL}${lat},${lng}`;
+	console.log(response.data.results[0].formatted_address);
+	return axios.get(weatherURL);
+}).then((response) => {
+	var temp = response.data.currently.temperature;
+	console.log('Temperature: ', temp);
+}).catch((e) => {
+	console.log(e.message);
+});
